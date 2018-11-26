aaac1ccf8df97e39ffed06f4e79873d5293526df
Spencer Ogden <spencer@spencerogden.com>
2018-11-25 21:14:31 -0500

    Update to Python 3 urllib

--- cdc_life_tables/cdc_life_tables.py
+++ cdc_life_tables/cdc_life_tables.py
@@ -15,7 +15,7 @@ Many states have Black, Male Black, and Female Black.
 
 import os
 from glob import glob
-import urllib2
+import urllib.request
 
 import pandas as pd
 
@@ -118,7 +118,7 @@ if n_life_table_csv_files != 426:
     for state in two_letter_abbrev:
         s = state.lower().replace(' ', '_')
         state_url = cdc_url + 'lewk4_{}.xlsx'.format(s)
-        url = urllib2.urlopen(state_url)
+        url = urllib.request.urlopen(state_url)
      
         xls = pd.ExcelFile(url)
         sheets = xls.sheet_names
